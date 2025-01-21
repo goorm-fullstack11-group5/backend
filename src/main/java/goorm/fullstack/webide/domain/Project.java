ca89288d7d8727db630bccfc0df18c2784e6e130
10cheon00 <10cheon00@gmail.com>
2025-01-21 14:31:01 +0900

    refactor: Audit에 의해 영향받는 컬럼들 분리

--- src/main/java/goorm/fullstack/webide/domain/Project.java
+++ src/main/java/goorm/fullstack/webide/domain/Project.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,8 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Project {
+public class Project extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
