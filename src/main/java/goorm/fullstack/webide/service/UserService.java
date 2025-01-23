0ceb3a10dfa64bc7dc8acc8c1623e107a8a59554
Nohbin2 <bean__2@naver.com>
2025-01-23 23:30:26 +0900

    feat: authentication

---
+++ src/main/java/goorm/fullstack/webide/service/UserService.java
@@ -0,0 +1,33 @@
+package goorm.fullstack.webide.service;
+
+import goorm.fullstack.webide.domain.User;
+import goorm.fullstack.webide.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(String username, String password, String email) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setEmail(email);
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
+
