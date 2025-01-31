38860a935dfe5d5cfe3385d664082a6509b364db
Kim Taekcheon <10cheon00@gmail.com>
2025-01-31 20:21:51 +0900

    refactor: getFileTree의 쿼리 호출 횟수 최적화 (#15)

--- src/main/java/goorm/fullstack/webide/dto/FileTreeNodeDto.java
+++ src/main/java/goorm/fullstack/webide/dto/FileTreeNodeDto.java
@@ -3,17 +3,18 @@ package goorm.fullstack.webide.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import goorm.fullstack.webide.domain.File;
+import java.util.ArrayList;
 import java.util.List;
 
-public record FileTreeNodeDto(int id, String name, Boolean isFolder, @JsonInclude(Include.NON_NULL) List<FileTreeNodeDto> files) {
-    public FileTreeNodeDto {
-        if (!isFolder) {
-            files = null;
-        }
+public record FileTreeNodeDto(int id, String name, Boolean isFolder,
+                              @JsonInclude(Include.NON_NULL) List<FileTreeNodeDto> files) {
+    public FileTreeNodeDto(File file) {
+        // todo: 파라미터가 폴더일 때 ArrayList를 만들게 되는데, runCode에서 지적받았던 것처럼
+        //  폴더 안에 하위 폴더나 파일이 엄청 많을 때 계속 array를 두 배로 늘리는 작업이 예상된다.
+        this(file.getId(), file.getName(), file.getIsFolder(), file.getIsFolder() ? new ArrayList<>() : null);
     }
 
-    public FileTreeNodeDto(File file) {
-        this(file.getId(), file.getName(), file.getIsFolder(),
-            file.getFiles().stream().map(FileTreeNodeDto::new).toList());
+    public void addFileTreeNodeDto(FileTreeNodeDto fileTreeNodeDto) {
+        files.add(fileTreeNodeDto);
     }
 }
--- src/main/java/goorm/fullstack/webide/service/ProjectService.java
+++ src/main/java/goorm/fullstack/webide/service/ProjectService.java
@@ -8,8 +8,9 @@ import goorm.fullstack.webide.dto.ProjectRequestDto;
 import goorm.fullstack.webide.repository.FileJpaRepository;
 import goorm.fullstack.webide.repository.ProjectRepository;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -51,16 +52,42 @@ public class ProjectService implements OwnableValidation {
     }
 
     public FileTreeNodeDto getFileTree(int projectId) {
+        // 프로젝트의 루트 폴더를 최상위로 두는 하위 폴더, 파일들을 전부 가져온다.
         Project project = projectRepository.findById(projectId)
             .orElseThrow(EntityNotFoundException::new);
         File rootFolder = project.getRootFolder();
-        return new FileTreeNodeDto(rootFolder);
+
+        return makeFileTree(fileJpaRepository.findAllFilesInFolder(rootFolder.getId()));
+    }
+
+    private FileTreeNodeDto makeFileTree(List<File> files) {
+        Map<Integer, FileTreeNodeDto> fileTreeNodeDtoMap = new HashMap<>();
+
+        // 먼저 조회된 모든 파일들을 FileTreeNodeDto로 변환하여 map에 저장한다.
+        for (File file : files) {
+            fileTreeNodeDtoMap.put(file.getId(), new FileTreeNodeDto(file));
+        }
+
+        // 그 다음 map에 추가한 값들을 이용하여 트리 구조를 생성한다.
+        FileTreeNodeDto fileTree = null;
+        for (File file : files) {
+            FileTreeNodeDto fileTreeNodeDto = fileTreeNodeDtoMap.get(file.getId());
+            // 파일의 부모가 없다는건 이 파일이 루트 폴더임을 의미
+            if (!file.hasParent()) {
+                fileTree = fileTreeNodeDto;
+                continue;
+            }
+            FileTreeNodeDto parentFileTreeNodeDto = fileTreeNodeDtoMap.get(file.getParent().getId());
+            parentFileTreeNodeDto.addFileTreeNodeDto(fileTreeNodeDto);
+        }
+        return fileTree;
     }
 
     @Override
     public boolean isOwner(int resourceId) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Project project = projectRepository.findById(resourceId).orElseThrow(EntityNotFoundException::new);
+        Project project = projectRepository.findById(resourceId)
+            .orElseThrow(EntityNotFoundException::new);
         return project.getUser().getUserId() == user.getUserId();
     }
 }
