ac07bb8c338bdd83398f2f8b59b4fe7b82e9eaf0
10cheon00 <10cheon00@gmail.com>
2025-02-01 21:14:30 +0900

    chore: codedeploy 로그에 시간을 제대로 출력하도록 변경

--- codedeploy/start.sh
+++ codedeploy/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-current_date_time="`date [+%Y-%m-%d %H:%M:%S]`";
-echo "$current_date_time Starting new application..." >> /home/ubuntu/log/deploy.log
-nohup java -jar /home/ubuntu/deployment/*.jar >> /home/ubuntu/log/app.log
-echo "$current_date_time Application started successfully." >> /home/ubuntu/log/deploy.log
+current_date_time() {
+    date +%Y-%m-%dT%H:%M:%S
+}
+echo "$(current_date_time) Starting new application..." >> /home/ubuntu/log/deploy.log
+nohup java -jar /home/ubuntu/deployment/*.jar > /home/ubuntu/log/app.log
+echo "$(current_date_time) Application started successfully." >> /home/ubuntu/log/deploy.log
