76d61703ca9ffd75bb24ff50301cb241a626287c
Kim Taekcheon <10cheon00@gmail.com>
2025-02-01 21:16:33 +0900

    CHORE: 현재 시간을 출력하도록 쉘 스크립트 변경 (#24)
    * chore: codedeploy 로그에 시간을 제대로 출력하도록 변경
    
    ---------
    
    Co-authored-by: Nohbin2 <98073212+Nohbin2@users.noreply.github.com>
    Co-authored-by: jseo0320 <jseo0320@naver.com>

--- codedeploy/deploy.sh
+++ codedeploy/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-current_date_time="`date [+%Y-%m-%d %H:%M:%S]`";
-echo "$current_date_time Granting execute permissions for new application..." >> /home/ubuntu/log/deploy.log
+current_date_time() {
+    date +%Y-%m-%dT%H:%M:%S
+}
+echo "$(current_date_time) Granting execute permissions for new application..." >> /home/ubuntu/log/deploy.log
 chmod +x /home/ubuntu/deployment/*.jar
 chmod +x /home/ubuntu/deployment/*.sh
-echo "$current_date_time Deployment preparation complete." >> /home/ubuntu/log/deploy.log
+echo "$(current_date_time) Deployment preparation complete." >> /home/ubuntu/log/deploy.log
