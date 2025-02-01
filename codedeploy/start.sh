8b4c6ff691aa45feab29eacee4312e1de264c3f1
Kim Taekcheon <10cheon00@gmail.com>
2025-02-01 21:22:14 +0900

    Update start.sh

--- codedeploy/start.sh
+++ codedeploy/start.sh
@@ -4,5 +4,5 @@ current_date_time() {
     date +%Y-%m-%dT%H:%M:%S
 }
 echo "$(current_date_time) Starting new application..." >> /home/ubuntu/log/deploy.log
-nohup java -jar /home/ubuntu/deployment/*.jar > /home/ubuntu/log/app.log
+nohup java -jar /home/ubuntu/deployment/*.jar > /home/ubuntu/log/app.log 2>&1 &
 echo "$(current_date_time) Application started successfully." >> /home/ubuntu/log/deploy.log
