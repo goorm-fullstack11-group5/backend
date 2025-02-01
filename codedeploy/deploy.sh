f5e98b9e3d85ebb76b7df0e09d856261177268e0
10cheon00 <10cheon00@gmail.com>
2025-02-01 20:01:19 +0900

    fix: codedeploy 파일 권한 추가

--- codedeploy/deploy.sh
+++ codedeploy/deploy.sh
@@ -2,5 +2,5 @@
 
 echo "Granting execute permissions for new application..."
 chmod +x /home/ubuntu/deployment/*.jar
-
+chmod +x /home/ubuntu/deployment/*.sh
 echo "Deployment preparation complete."
